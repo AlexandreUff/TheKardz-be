3c8352b671676825d2ae497a600d06c1acfadd4f
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-28 17:03:34 -0300

    chore: set a structure to create user

--- models/User.js
+++ models/User.js
@@ -3,17 +3,35 @@ const conn = require('../db/conn')
 class User {
     static UserConnection = conn.db().collection("user")
 
-    static async create(name){
-        const user = await this.UserConnection.insertOne({
-            name,
-            victories: 0,
-            loses: 0,
-            hall: "",
-            ping: true,
-            isFighting: false,
-            consecutives: 0,
-            lineNumber: 0
-        })
+    static async create(name, hallNumber){
+        let user
+
+        const isThereThisUser = await this.UserConnection.findSuchUserInHall(name, hallNumber)
+
+        if(!isThereThisUser){
+            user = await this.UserConnection.insertOne({
+                name,
+                victories: 0,
+                loses: 0,
+                hall: "1111",
+                ping: true,
+                isFighting: false,
+                consecutives: 0,
+                lineNumber: 0
+            })
+        } else {
+            return {
+                isCreated: false,
+                message: "Nome de usuário já em uso nesta sala.",
+                data: null
+            }
+        }
+
+        return {
+            isCreated: true,
+            message: "Usuário criado com sucesso.",
+            data: user
+        }
     }
 
     static async findSuchUserInHall(name, hallNumber){
