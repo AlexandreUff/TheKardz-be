ecc4ef6f3a85bc22e252cfef25d54ee1bba024a6
AlexandreUff <alexandre_magno@id.uff.br>
2023-07-04 16:11:15 -0300

    chore: remove unusual code

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const hallRoutes = require('./routes/hallRoutes')
 const userRoutes = require('./routes/userRoutes')
 const gameRoutes = require('./routes/gameRoutes')
 
-server.use((req, res, next) => {
+/* server.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  });
+  }); */
 
 //read body
 server.use(
