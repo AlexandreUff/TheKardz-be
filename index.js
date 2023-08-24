a6803cc886abe79d868ea562006e652af73a482f
AlexandreUff <alexandre_magno@id.uff.br>
2023-08-23 22:43:55 -0300

    feat: set cors configs

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require('express')
 const server = express()
 const SocketConnectionStart = require('./services/SocketService')
 const cors = require('cors')
+require('dotenv').config()
 
 const hallRoutes = require('./routes/hallRoutes')
 const userRoutes = require('./routes/userRoutes')
 const gameRoutes = require('./routes/gameRoutes')
 
-/* server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000' && process.env.URL_CLI_PROD);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  }); */
+  });
 
 //read body
 server.use(
